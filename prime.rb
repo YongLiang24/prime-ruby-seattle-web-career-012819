538b63ebf3f6f7b4fee353a4a8930aaab38db269
yong liang <yongliang24@gmail.com>
2019-01-20 00:48:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# Add  code here
+def prime?(n)
+  if n <=1
+    return false;
+  end
+n_count = 2...n
+    for num in n_count
+      if(n%num ==0)
+        return false
+      end
+    end
+    return true;
+end
